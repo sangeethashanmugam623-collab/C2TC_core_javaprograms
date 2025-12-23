b9d0076253edd89e854dc072fb0b8535a3fbe8a7
Sangeetha Shanmugam <sangeethashanmugam623@gmail.com>
2025-12-23 20:46:07 +0530

    Add new package NewPackageName

---
+++ src/com/tnsif/Exceptionhandling/WithoutExceptionHandling.java
@@ -0,0 +1,17 @@
+package com.tnsif.Exceptionhandling;
+
+public class WithoutExceptionHandling {
+
+	public static void main(String[] args) {
+		
+				System.out.println("the program continues....");
+				int data=100/0;
+				System.out.println("the program continues....");
+			}
+			
+		}
+
+
+	
+
+
